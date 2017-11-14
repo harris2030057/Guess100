b7bed8d434bb24191354696b8bb6976745431273
Harris <harris2030057@gmail.com>
2017-11-14 10:47:48 +0800

    Guess

--- app/src/main/java/com/harris/user/guess100/MainActivity.java
+++ app/src/main/java/com/harris/user/guess100/MainActivity.java
@@ -1,11 +1,13 @@
 package com.harris.user.guess100;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -26,10 +28,31 @@ Random random = new Random();
     }
     public void findViews(View view){
         TextView secret = (TextView) findViewById(R.id.secret);
-        TextView info = (TextView) findViewById(R.id.number);
-        EditText number = (EditText) findViewById(R.id.number);
+        TextView info = (TextView) findViewById(R.id.info);
+        EditText number = (EditText) findViewById(R.id.info);
         Button send = (Button) findViewById(R.id.send);
+       int n = Integer.parseInt(number.getText().toString());
+       boolean b = true ;
 
-
+        if (n>max || n<min){
+            b=false;
+            /*Toast.makeText(this,)*/
+        }
+        if(b==true){
+            if(n==r){
+                new AlertDialog.Builder(this)
+                        .setMessage("你答對了")
+                        .setPositiveButton("確定",null)
+                        .show();
+            }
+            if(n>r){
+                max = n;
+                        Toast.makeText(this,"請輸入："+min+"-"+max,Toast.LENGTH_LONG).show();
+            }
+            if(n<r){
+                n=min;
+                Toast.makeText(this,"請輸入："+min+"-"+max,Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
