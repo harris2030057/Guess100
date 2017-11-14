2722ae74afe4b40beccced5ec669dd935153b399
Harris <harris2030057@gmail.com>
2017-11-14 10:20:52 +0800

    findViews

--- app/src/main/java/com/harris/user/guess100/MainActivity.java
+++ app/src/main/java/com/harris/user/guess100/MainActivity.java
@@ -2,12 +2,27 @@ package com.harris.user.guess100;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+Random random = new Random();
+    int min = 1 ;
+    int max = 100;
+    int r = random.nextInt(100)+1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void findViews(View view){
+
+
     }
 }
