03a4de24d3c4e6e03654a7dc508828eb7f38e363
Harris <harris2030057@gmail.com>
2017-11-14 10:28:55 +0800

    findViews

--- app/src/main/java/com/harris/user/guess100/MainActivity.java
+++ app/src/main/java/com/harris/user/guess100/MainActivity.java
@@ -3,6 +3,9 @@ package com.harris.user.guess100;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -22,6 +25,10 @@ Random random = new Random();
 
     }
     public void findViews(View view){
+        TextView secret = (TextView) findViewById(R.id.secret);
+        TextView info = (TextView) findViewById(R.id.number);
+        EditText number = (EditText) findViewById(R.id.number);
+        Button send = (Button) findViewById(R.id.send);
 
 
     }
